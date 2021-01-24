851d52e04940f4675f21a4ad186924a4c1f106a5
AnLyn <7anlyn@gmail.com>
2021-01-24 23:02:49 +0900

    2021 01 24 AlarmApp create GetAllAlarmUseCase

--- app/src/main/java/com/anlyn/alarmwheater/presentation/ui/DisplayAlarmViewModel.kt
+++ app/src/main/java/com/anlyn/alarmwheater/presentation/ui/DisplayAlarmViewModel.kt
@@ -1,2 +1,14 @@
 package com.anlyn.alarmwheater.presentation.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.anlyn.domain.models.AlarmEntity
+
+class DisplayAlarmViewModel : ViewModel(){
+    lateinit var alarmCacheLiveData : LiveData<List<AlarmEntity>>
+
+
+    fun getAlarmEntity(){
+//        val useCase = AddA()
+    }
+}
