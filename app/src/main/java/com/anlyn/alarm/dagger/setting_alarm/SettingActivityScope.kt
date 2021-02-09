2eb99cd49aad3c5953fd1b8928a8422798534ee5
AnLyn <7anlyn@gmail.com>
2021-02-09 23:39:20 +0900

    2021-02-09 btn click event all binding with layout

---
+++ app/src/main/java/com/anlyn/alarm/dagger/setting_alarm/SettingActivityScope.kt
@@ -0,0 +1,7 @@
+package com.anlyn.alarm.dagger.setting_alarm
+
+import javax.inject.Scope
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class SettingActivityScope {}
--- app/src/main/java/com/anlyn/alarm/presentation/ui/alarmsetting/SettingVMFactory.kt
+++ app/src/main/java/com/anlyn/alarm/presentation/ui/alarmsetting/SettingVMFactory.kt
@@ -2,10 +2,12 @@ package com.anlyn.alarm.presentation.ui.alarmsetting
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.anlyn.alarm.databinding.ActivitySettingBinding
 import com.anlyn.domain.usercase.AddAlarmUseCase
 
-class SettingVMFactory constructor(val addAlarmUseCase: AddAlarmUseCase) : ViewModelProvider.Factory{
+class SettingVMFactory constructor(private val addAlarmUseCase: AddAlarmUseCase,
+                                   private val binding:ActivitySettingBinding) : ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return SettingViewModel(addAlarmUseCase) as T
+        return SettingViewModel(addAlarmUseCase,binding) as T
     }
 }
